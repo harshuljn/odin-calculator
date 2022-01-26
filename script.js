5852fa9462785750433db83d2f1f4aacc56c166d
Harshul Jain <harshuljain2047@gmail.com>
2022-01-26 19:51:03 +0530

    Add Clear btn and refactor js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operation = "+";
+let operand = "";
+let result = "0";
+
 function operate(a, b, operation) {
 	if (!operand) return a;
 	if (operation === "+") return a + b;
@@ -7,36 +11,49 @@ function operate(a, b, operation) {
 	else if (operation === "=") return a;
 }
 
-let operation = "+";
-let operand = "";
-let result = "0";
-
 function operatorOnClickEventListener(operator) {
 	result = operate(+result, +operand, operation);
 	operand = "";
 	operation = operator.innerText;
 	document.querySelector(".result").textContent = result;
 	document.querySelector(".operand").textContent = operand;
-	console.log(result);
+	// console.log(result);
 }
 
-const operators = Array.from(document.querySelectorAll(".operator"));
-operators.forEach((operator) =>
-	operator.addEventListener("click", () =>
-		operatorOnClickEventListener(operator)
-	)
-);
+function resetCalculator() {
+	operation = "+";
+	operand = "";
+	result = "0";
+	document.querySelector(".result").textContent = "";
+	document.querySelector(".operand").textContent = "0";
+	// console.log("reset");
+}
 
-const digits = Array.from(document.querySelectorAll(".digit"));
-digits.forEach((digit) =>
-	digit.addEventListener("click", () => {
-		operand += digit.innerText;
-		document.querySelector(".operand").textContent = operand;
-	})
-);
+function setOperatorEventListeners() {
+	const operators = Array.from(document.querySelectorAll(".operator"));
+	operators.forEach((operator) =>
+		operator.addEventListener("click", () =>
+			operatorOnClickEventListener(operator)
+		)
+	);
 
-const calc = document.querySelector(".calc");
-calc.addEventListener("click", () => operatorOnClickEventListener(calc));
+	const calc = document.querySelector(".calc");
+	calc.addEventListener("click", () => operatorOnClickEventListener(calc));
 
-const resultScreen = document.querySelector(".result");
-resultScreen.addEventListener();
+	const clr = document.querySelector(".digits .clear");
+	clr.addEventListener("click", resetCalculator);
+}
+
+function setDigitEventListeners() {
+	const digits = Array.from(document.querySelectorAll(".digit"));
+	digits.forEach((digit) =>
+		digit.addEventListener("click", () => {
+			operand += digit.innerText;
+			document.querySelector(".operand").textContent = operand;
+		})
+	);
+}
+
+setOperatorEventListeners();
+setDigitEventListeners();
+resetCalculator();
